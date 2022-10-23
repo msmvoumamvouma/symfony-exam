baf320a84c7dc4ed34dbccf3983658517960c89b
msmvoumamvouma <56298431+msmvoumamvouma@users.noreply.github.com>
2022-10-23 20:25:36 +0200

    code: adding author entity, author repository, migration

---
+++ src/Entity/Author.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuthorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AuthorRepository::class)]
+class Author
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstName = null;
+
+    #[ORM\OneToMany(mappedBy: 'author', targetEntity: Book::class)]
+    private ?ArrayCollection $books;
+
+    public function __construct()
+    {
+        $this->books = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getBooks(): ?ArrayCollection
+    {
+        return $this->books;
+    }
+
+    public function setBooks(?ArrayCollection $books): self
+    {
+        $this->books = $books;
+
+        return $this;
+    }
+}
--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -8,13 +8,13 @@ use Doctrine\ORM\Mapping as ORM;
 use Overblog\GraphQLBundle\Annotation as GQL;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
-#[GQL\Type(name: "Book")]
+#[GQL\Type(name: 'Book')]
 class Book
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[GQL\Field(type: "ID")]
+    #[GQL\Field(type: 'ID')]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
@@ -24,6 +24,10 @@ class Book
     #[ORM\Column(type: Types::TEXT)]
     #[GQL\Field]
     private ?string $resume = null;
+
+    #[ORM\ManyToOne(targetEntity: Author::class)]
+    #[ORM\JoinColumn(name: "author_id")]
+    private ?Author $author = null;
 
     public function getId(): ?int
     {
@@ -50,6 +54,18 @@ class Book
     public function setResume(string $resume): self
     {
         $this->resume = $resume;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?Author
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?Author $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
