a27c5a1b43eed747ce48f72761d28c408561b8eb
msmvoumamvouma <56298431+msmvoumamvouma@users.noreply.github.com>
2022-10-23 23:33:21 +0200

    chore: remove sensio_framework_extra dependency

--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,4 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
-
