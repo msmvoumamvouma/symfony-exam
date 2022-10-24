2307cd5058da5b507e287315d4229532fcd7329e
msmvoumamvouma <56298431+msmvoumamvouma@users.noreply.github.com>
2022-10-24 15:30:45 +0200

    code: adding route to add author and book without validation data

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Author;
 use App\Entity\GroupName;
+use App\Service\HandleAuthor;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,5 +23,14 @@ class AuthorController extends AbstractController
         );
 
         return $this->json($author, 200, [], $context);
+    }
+
+    #[Route('/author', name: 'add_authors', methods: ['POST'])]
+    public function add(SerializerInterface $serializer, Request $request, HandleAuthor $handleAuthor): JsonResponse
+    {
+        $author = $serializer->deserialize($request->getContent(), Author::class, 'json', GroupName::GROUPS_ONLY_WRITE);
+        $handleAuthor->addAuthor($request->getContent());
+
+        return $this->json($author, 200, [], GroupName::GROUPS_ONLY_WRITE);
     }
 }
