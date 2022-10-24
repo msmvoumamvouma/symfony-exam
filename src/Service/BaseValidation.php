5d37d0e323154d56aa05dcde7ec6e1c341ee8ad7
msmvoumamvouma <56298431+msmvoumamvouma@users.noreply.github.com>
2022-10-24 18:11:56 +0200

    code: adding base class validation

---
+++ src/Service/BaseValidation.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class BaseValidation
+{
+    protected ValidatorInterface $validator;
+    protected string $className;
+    private array $groups;
+
+    public function __construct(ValidatorInterface $validator, string $className, array $groups)
+    {
+        $this->validator = $validator;
+        $this->className = $className;
+        $this->groups = $groups;
+    }
+
+    private function readViolations(ConstraintViolationListInterface $violations): array
+    {
+        $result = [];
+        /**
+         * @var ConstraintViolation $violation
+         */
+        foreach ($violations as $violation) {
+            $result[$violation->getPropertyPath()] = sprintf('%s %s', $violation->getPropertyPath(), $violation->getMessage());
+        }
+
+        return $result;
+    }
+
+    private function executeValidationWith($value, $constraints = null): ConstraintViolationListInterface
+    {
+        return $this->validator->validate($value, $constraints, $this->groups);
+    }
+
+    private function getViolationConstraint(object $theObject ): ConstraintViolationListInterface
+    {
+
+        return $this->executeValidationWith($theObject);
+    }
+
+    public function validate(object $theObject): array
+    {
+        $violations = $this->getViolationConstraint($theObject );
+
+       return  $this->readViolations( $violations);
+    }
+}
