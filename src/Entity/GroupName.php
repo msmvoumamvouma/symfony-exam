832068baafed063124ac2b80cfa974ed85c14e0f
msmvoumamvouma <56298431+msmvoumamvouma@users.noreply.github.com>
2022-10-24 15:38:57 +0200

    chore: make indentation

--- src/Entity/GroupName.php
+++ src/Entity/GroupName.php
@@ -6,5 +6,5 @@ class GroupName
 {
     public const READ = 'read';
     public const WRITE = 'write';
-    const GROUPS_ONLY_WRITE =  ['groups' => [GroupName::WRITE]];
+    public const GROUPS_ONLY_WRITE = ['groups' => [GroupName::WRITE]];
 }
