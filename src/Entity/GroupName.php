11f6a5a966ed49985f7bc0f24469b3eb820eced0
msmvoumamvouma <56298431+msmvoumamvouma@users.noreply.github.com>
2022-10-26 05:50:37 +0200

    refactor: wrap context group FILTERABLE

--- src/Entity/GroupName.php
+++ src/Entity/GroupName.php
@@ -8,4 +8,5 @@ class GroupName
     public const WRITE = 'write';
     public const FILTERABLE = 'filter';
     public const GROUPS_ONLY_WRITE = ['groups' => [GroupName::WRITE]];
+    public const GROUPS_ONLY_FILTERABLE = ['groups' => [GroupName::FILTERABLE]];
 }
